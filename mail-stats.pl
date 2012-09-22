f3369110cd9fe36a258e9fde454f698cf2c87f74
Moshe Nahmias <moshegrey@gmail.com>
2012-09-22 04:08:34 +0300

    doing some of gaal suggestions and starting with OOP

--- mail-stats.pl
+++ mail-stats.pl
@@ -17,8 +17,7 @@ Make the statistics for your mailgroup. For now it uses C<wget> and C<gzip> so I
 
 =cut
 
-my ( @files, @file, @name, @names, $year, $name, $num, $firstname, $first, $sec, $third, $forth,
-$fifth, $six, $seven, $eight, $nin, $ten, %post, %names );
+my ( @files, @names, $year, $num, %names );
 our $VERSION = 0.09;
 my $file = "file.txt";
 
@@ -42,123 +41,124 @@ print "Whats the URL of your mails archive? ";
 my $url = <STDIN>;
 chomp $url;
 
+print "Percent of how many participant do you want to calculate? ";
+my $number = <STDIN>;
+chomp $number;
+
 # the data will be stored temporarily untill the script is done.
 
 my $dir = tempdir( CLEANUP => 1 );
 
-
-#http://mail.perl.org.il/pipermail/perl/
 # the file we create for use to get the files of the archives we need to make stats of.
 
-getstore("$url", "$dir/mail.txt") or die "Unable to get page: $!";
+html {
+	getstore("$url", "$dir/mail.txt") or die "Unable to get page: $!";
 
-open my $html, "<", "$dir/mail.txt" or die "can't open mail.txt: $! ";
+	open my $html, "<", "$dir/mail.txt" or die "can't open mail.txt: $! ";
 
-# making the info in an easy way to read and use.
+	# making the info in an easy way to read and use.
 
-while (<$html>) {
-	if ( $_ =~ /gzip/i ) {
-		s/<td><A href="/http:\/\/mail.perl.org.il\/pipermail\/perl\//;
-		s/"\>\[ Gzip'd Text \d{1,6} KB \]\<\/a\>\<\/td>//;
-		s/"\>\[ Gzip'd Text \d{1,6} bytes \]\<\/a\>\<\/td>//;
-		push @files, $_;
+	while (<$html>) {
+		if ( $_ =~ /gzip/i ) {
+			s/<td><A href="/$url\//;
+			s/"\>\[ Gzip'd Text \d{1,6} KB \]\<\/a\>\<\/td>//;
+			s/"\>\[ Gzip'd Text \d{1,6} bytes \]\<\/a\>\<\/td>//;
+			push @files, $_;
+		}
 	}
+
+	close $html or die "can't close mail.txt: $! ";
 }
 
-close $html;
+down {
+	# downloading the needed files and extracting them.
 
-# downloading the needed files and extracting them.
-
-for ( @files ) {
-	system("wget -P $dir -nc $_") if $_ =~ /$year/;
+	for ( @files ) {
+		system("wget -P $dir -nc $_") if $_ =~ /$year/;
+	}
+	system("gzip -d $dir/*.gz");
 }
 
-system("gzip -d $dir/*.gz");
+merge {
+	my @file;
+	# reading the files.
 
-# reading the files.
+	opendir my $DIR, "$dir" or die "can't open direcory $dir: $! ";
+	open my $FILE05, ">", "$dir$file" or die "can't open file $file: $! ";
 
-opendir my $DIR, "$dir" or die "can't open direcory $dir: $! ";
-open my $FILE05, ">", "$dir$file" or die "can't open file $file: $! ";
+	while ( my $file = readdir($DIR) ) {
+		next if $file eq "." or $file eq "..";
+		push @file, $file;
+	}
 
-while ( my $file = readdir($DIR) ) {
-	next if $file eq "." or $file eq "..";
-	push @file, $file;
+	# writing the needed info for the stats.
+	
+	for ( @file ) {
+		open my $FILE, "<", "$dir/$_" or die "can't open $_ : $! ";
+		if ( $_ =~ /$year/ ) {
+			while ( <$FILE> ) {
+				say $FILE05 $_;
+			}
+		}
+	}
+	close $FILE05 or die "can't close file: $! ";
 }
 
-# writing the needed info for the stats.
+var {
+	# reading the file to populate the variables for the stats.
 
-for ( @file ) {
-	open my $FILE, "<", "$dir/$_" or die "can't open $_ : $! ";
-	if ( $_ =~ /$year/ ) {
-		while ( <$FILE> ) {
-			say $FILE05 $_;
+	open my $FI05, "<", "$dir$file" or die "can't open file $file: $! ";
+
+	my %post;
+	$num = 0;
+	while ( <$FI05> ) {
+		if ( $_ =~ /\AFrom:/ and $_ =~ /\(/ ) {
+			s/\)//;
+			my @name = split /\(/, $_;
+			chomp @name;
+			my $name = $name[1];
+			push @names, $name unless (exists $post{$name});
+			$names{$name} = 0 unless exists $names{$name};
+			if ( exists $names{$name} ) {
+				$names{$name}++;
+			}
+			$post{$name} = $names[$num];
+			$num++;
 		}
 	}
 }
 
-close $FILE05 or die "can't close file: $! ";
+stats {
+	my @sort = sort { $a <=> $b } ( values %names );
 
-# reading the file to populate the variables for the stats.
+	my $n = @names;
 
-open my $FI05, "<", "$dir$file" or die "can't open file $file: $! ";
+	# making the statistics.
 
-$num = 0;
-while ( <$FI05> ) {
-	if ( $_ =~ /\AFrom:/ and $_ =~ /\(/ ) {
-		s/\)//;
-		@name = split /\(/, $_;
-		chomp @name;
-		$name = $name[1];
-		push @names, $name unless (exists $post{$name});
-		$names{$name} = 0 unless exists $names{$name};
-		if ( exists $names{$name} ) {
-			$names{$name}++;
+	my $num_of_mails = 0;
+
+	for ( 1 .. $number ) {
+		$num_of_mails = $sort[-$_] + $num_of_mails;
+	}
+
+	my $percent_of_people = 100 * $num_of_mails / $num;
+	my $percent_first = 100 * $sort[-1] / $num;
+	# writing the statistics on the terminal.
+
+	my $firstname;
+
+	for ( keys %names ) {
+		if ( $names{$_} == $sort[-1] ) {
+			$firstname = $_;
+			last;
 		}
-		$post{$name} = $names[$num];
-		$num++;
 	}
+
+	say "we had $n participants and they sent $num posts in $year";
+	print "the participant with the most posts is $firstname with ";
+	printf( "%.1f", $percent_first );
+	say "% of the posts";
+	print "the top $number posters made about ";
+	printf("%.1f", $percent_of_people);
+	say "% of the posts";
 }
-
-my $n = @names;
-
-my @sort = sort { $a <=> $b } ( values %names );
-
-for ( keys %names ) {
-	if ( $names{$_} == $sort[-1] ) {
-		$firstname = $_;
-		$first = $names{$_};
-	} if ( $names{$_} == $sort[-2] ) {
-		$sec = $names{$_};
-	} if ( $names{$_} == $sort[-3] ) {
-		$third = $names{$_};
-	} if ( $names{$_} == $sort[-4] ) {
-		$forth = $names{$_};
-	} if ( $names{$_} == $sort[-5] ) {
-		$fifth = $names{$_};
-	} if ( $names{$_} == $sort[-6] ) {
-		$six = $names{$_};
-	} if ( $names{$_} == $sort[-7] ) {
-		$seven = $names{$_};
-	} if ( $names{$_} == $sort[-8] ) {
-		$eight = $names{$_};
-	} if ( $names{$_} == $sort[-9] ) {
-		$nin = $names{$_};
-	} if ( $names{$_} == $sort[-10] ) {
-		$ten = $names{$_};
-	}
-}
-
-# making the statistics.
-
-my $percentten = 100 * ( $first + $sec + $third + $forth + $fifth + $six + $seven + $eight + $nin + $ten ) / $num;
-my $percentfirst = 100 * $first / $num;
-
-# writing the statistics on the terminal.
-
-say "we had $n participants and they sent $num posts in $year";
-print "the participant with the most posts is $firstname with ";
-printf( "%.1f", $percentfirst );
-say "% of the posts";
-print "the top 10 posters made about ";
-printf("%.1f", $percentten);
-say "% of the posts";
