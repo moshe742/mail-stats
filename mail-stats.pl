e8e20a32c5cddefbaa8be46ca045668b688ba225
Moshe Nahmias <moshegrey@gmail.com>
2012-12-27 00:34:55 +0200

    another work done

--- mail-stats.pl
+++ mail-stats.pl
@@ -6,10 +6,12 @@ use v5.10;
 use Lib::Download;
 use Lib::Input;
 use Lib::Html;
+#use Lib::Mail;
 use Lib::Extract;
 use Lib::Merge;
 use Lib::Var;
 use Lib::Stats;
+use Lib::Display;
 use File::Spec;
 use File::Temp qw(tempdir);
 
@@ -51,25 +53,39 @@ of the data.
 
 =cut
 
+my $input = Lib::Input->new();
+my $html = Lib::Html->new();
+my $down = Lib::Download->new();
+my $extract = Lib::Extract->new();
+my $merge = Lib::Merge->new();
+my $var = Lib::Var->new();
+my $stats = Lib::Stats->new();
+my $display = Lib::Display->new();
+
 # Input from the user of the program.
-#input();
-my ( $year, $input_url, $number ) = Lib::Input->input(@ARGV);
+my ( $year, $input_url, $number ) = $input->input(@ARGV);
+
+#my $mail = Lib::Mail->new(
+#								year => $year,
+#								input_url => $input_url,
+#								);
 
 # Finding the files we need to download and getting ready to download them.
-my @files = Lib::Html->html( $year, $input_url, $number );
+my @files = $html->html( $year, $input_url );
 
 # Downloading the files we want to check.
-Lib::Download->download( $dir, $input_url, @files );
-#download();
+$down->download( $dir, $input_url, @files );
 
 # Extracting the downloaded files.
-Lib::Extract->extract($dir);
+$extract->extract($dir);
 
 # Merging the data to 1 big file for easier retrival.
-Lib::Merge->merge($year, $dir, $file_path);
+$merge->merge($year, $dir, $file_path);
 
 # Retriving the info we want for our stats.
-my ( $number_of_posts, $number_of_participants, %names ) = Lib::Var->var($file_path);
+my ( $number_of_posts, $number_of_participants, %names ) = $var->var($file_path);
 
 # Calculating the stats.
-Lib::Stats->stats($number, $number_of_participants, $number_of_posts, $year, %names);
+my @stats = $stats->stats_year($number, $number_of_participants, $number_of_posts, $year, %names);
+
+$display->display_year( @stats, $year );
